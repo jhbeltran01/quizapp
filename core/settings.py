518b88b0114f4f6a31b8a29c6eb9f4aaacb13e7b
jhbeltran01 <jhbeltran2001>
2022-09-19 15:02:21 +0800

    configure settings.py for database

--- core/settings.py
+++ core/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quizapp',
+        'USER': 'johbeltran',
+        'PASSWORD': 'admin',
+        'HOST': '',
+        'PORT': '5432'
     }
 }
 
