a1709b775a6dca3c802d9a3d2c80411d9e8125b7
jhbeltran01 <jhbeltran2001>
2022-09-19 16:40:40 +0800

    define Student model extending AbstractUser

--- project_apps/login/models.py
+++ project_apps/login/models.py
@@ -1,13 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
-User._meta.get_field('email')._unique = True
-User._meta.get_field('email').null = False
-User._meta.get_field('email').blank = False
+class Student(AbstractUser):
+	username = models.CharField(unique=False, max_length=50)
+	email = models.EmailField(_('Email'), unique=True, blank=False, null=False)
+	student_id = models.CharField(max_length=20, null=True, blank=True)
+	course = models.CharField(max_length=20, null=True, blank=True)
+	first_name = models.CharField(max_length=200, null=False, blank=False)
+	last_name = models.CharField(max_length=200, null=False, blank=False)
+	middle_name = models.CharField(max_length=200, null=False, blank=False, default='')
 
-User._meta.get_field('first_name').null = False
-User._meta.get_field('first_name').blank = False
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username', 'last_name', 'first_name', 'middle_name']
 
-User._meta.get_field('last_name').null = False
-User._meta.get_field('last_name').blank = False
+
+	def __str__(self):
+		return self.username
