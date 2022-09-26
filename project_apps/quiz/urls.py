fb15fe4afe01bad05b02b667509b80b8fcce8f4a
jhbeltran01 <jhbeltran2001>
2022-09-26 19:45:47 +0800

    add comment and rename RedirectToUpdate to RedirectToFill

--- project_apps/quiz/urls.py
+++ project_apps/quiz/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views.HomePageView import HomePageView
 from .views.CreateRoomView import CreateRoomView
 from .views.JoinRoomView import JoinRoomView
-from .views.RedirectToUpdateStudentDataOrJoinRoomView import RedirectToUpdateStudentDataOrJoinRoomView
+from .views.RedirectToFillStudentDataOrJoinRoomView import RedirectToFillStudentDataOrJoinRoomView
 from .views.CreateStudent import CreateStudent
 from .views.TestView import TestView
 from .views.QuestionView import QuestionView
@@ -17,7 +17,7 @@ urlpatterns = [
 	path('', HomePageView.as_view(), name='home'),
 	path('create-room/', CreateRoomView.as_view(), name='create-room'),
 	path('join-room/', JoinRoomView.as_view(), name='join-room'),
-	path('check-data/',RedirectToUpdateStudentDataOrJoinRoomView.as_view(), name='check-data'),
+	path('check-data/',RedirectToFillStudentDataOrJoinRoomView.as_view(), name='check-data'),
 	path('fill-user-data/<str:pk>', CreateStudent.as_view(), name='fill-student-data'),
 	path('tests/<int:room_id>/', TestView.as_view(), name='test'),
 	path('questions/<int:room_id>/<int:test_id>', QuestionView.as_view(), name='question'),
--- project_apps/quiz/views/RedirectToFillStudentDataOrJoinRoomView.py
+++ project_apps/quiz/views/RedirectToFillStudentDataOrJoinRoomView.py
@@ -4,8 +4,11 @@ from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ObjectDoesNotExist
 
-
-class RedirectToUpdateStudentDataOrJoinRoomView(LoginRequiredMixin, generic.RedirectView):
+"""
+	If the user is a student, redirect to join room page,
+	else redirect to student form page
+"""
+class RedirectToFillStudentDataOrJoinRoomView(LoginRequiredMixin, generic.RedirectView):
 	def get(self, *args, **kwargs):
 		if hasattr(self.request.user, 'student'):
 			return HttpResponseRedirect(reverse('quiz:join-room'))
