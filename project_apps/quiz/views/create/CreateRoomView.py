7bccf7cb9578c2d119319e38e0f2549bd3edd2c9
jhbeltran01 <jhbeltran2001>
2022-09-26 20:06:10 +0800

    put create views in one folder

---
+++ project_apps/quiz/views/create/CreateRoomView.py
@@ -0,0 +1,23 @@
+from django.views import generic
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from ...models import Room
+from ...forms.CreateRoomForm import CreateRoomForm
+
+class CreateRoomView(LoginRequiredMixin, generic.CreateView):
+	model = Room
+	form_class = CreateRoomForm
+	template_name = 'quiz/create-room.html'
+
+
+	def form_valid(self, form):
+		new_room = self.request.user.room_set.create(
+			name=form.cleaned_data['name'], 
+			room_code=form.cleaned_data['room_code'], 
+			is_private=form.cleaned_data['is_private'])
+
+		new_room.save()
+
+		return HttpResponseRedirect(reverse('quiz:test', args=(new_room.id,)))
