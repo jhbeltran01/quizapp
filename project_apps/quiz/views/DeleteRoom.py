b476026b63f4e0e036411455dfdc9a5aae3590e3
jhbeltran01 <jhbeltran2001>
2022-09-26 19:41:45 +0800

    refactor DeleteRoom.py

--- project_apps/quiz/views/DeleteRoom.py
+++ project_apps/quiz/views/DeleteRoom.py
@@ -5,5 +5,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class DeleteRoom(LoginRequiredMixin, generic.DeleteView):
   def get(self, request, *args, **kwargs):
-    self.request.user.room_set.get(pk=kwargs['room_id']).delete()
+    request.user.room_set.get(pk=kwargs['room_id']).delete()
     return HttpResponseRedirect(reverse('quiz:home'))
