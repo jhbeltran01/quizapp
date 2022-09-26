a0ee2c268ca9e6713bd7bfdd2dfd19f1886ce05d
jhbeltran01 <jhbeltran2001>
2022-09-26 16:37:03 +0800

    make student ID unique

--- project_apps/login/models.py
+++ project_apps/login/models.py
@@ -18,9 +18,9 @@ class CustomUser(AbstractUser):
 
 class Student(models.Model):
   user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
-  student_id = models.CharField(max_length=20, null=True, blank=True)
+  student_id = models.CharField(max_length=20, null=True, blank=True, unique=True)
   course = models.CharField(max_length=20, null=True, blank=True)
   
   
   def __str__(self):
-    return self.username
+    return self.user.username
